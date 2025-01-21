9cd481f87c8084d0e7ced59323cd63a069da0ade
Josue Ruiz <JosueRuiz.com@gmail.com>
2025-01-21 18:31:08 +0100

    Initial Commit

---
+++ AppPerros/AppPerrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPerrosApp.swift
+//  AppPerros
+//
+//  Created by Tardes on 21/1/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPerrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPerros/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppPerros
+//
+//  Created by Tardes on 21/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
