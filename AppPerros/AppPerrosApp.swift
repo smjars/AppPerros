782e9edc201f0231b4d94927fcbd90aab9c9771c
Josue Ruiz <JosueRuiz.com@gmail.com>
2025-01-22 18:29:23 +0100

    tratando de agregar el login con google pero da error

--- AppPerros/AppPerrosApp.swift
+++ AppPerros/AppPerrosApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Tardes on 21/1/25.
 //
-
+/*
 import SwiftUI
 import Firebase
 
@@ -29,4 +29,31 @@ struct AppPerrosApp: App {
       }
     }
 }
+*/
+import SwiftUI
+import Firebase
+import GoogleSignIn
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+}
+
+@main
+struct AppPerrosApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
--- AppPerros/Views/LoginView.swift
+++ AppPerros/Views/LoginView.swift
@@ -15,98 +15,6 @@ struct LoginView: View {
     @State private var password: String = ""
     @State private var isLoginMode: Bool = true
     @State private var errorMessage: String = ""
-    
-    var body: some View {
-        VStack {
-            Picker(selection: $isLoginMode, label: Text("Modo Registro")) {
-                Text("Iniciar Sesión").tag(true)
-                Text("Registrarse").tag(false)
-            }.pickerStyle(SegmentedPickerStyle())
-                .padding()
-            
-            TextField("Email", text: $email)
-                .keyboardType(.emailAddress)
-                .autocapitalization(.none)
-                .padding()
-            
-            SecureField("Contraseña", text: $password)
-                .padding()
-            
-            Button(action: {
-                handleAction()
-            }) {
-                Text(isLoginMode ? "Iniciar Sesión" : "Registrarse")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .padding()
-                    .frame(maxWidth: .infinity)
-                    .background(Color.blue)
-                    .cornerRadius(10)
-            }
-            .padding()
-            
-            Text(errorMessage)
-                .foregroundColor(.red)
-                .padding()
-        }
-        .padding()
-    }
-    
-    private func handleAction() {
-        if isLoginMode {
-            loginUser()
-        } else {
-            signUpUser()
-        }
-    }
-    
-    private func loginUser() {
-        Auth.auth().signIn(withEmail: email, password: password) { result, error in
-            if let error = error {
-                errorMessage = "Falló iniciar sesión: \(error.localizedDescription)"
-                return
-            }
-            // Manejar inicio de sesión exitoso
-            errorMessage = "Inicio de sesión exitoso"
-        }
-    }
-    
-    private func signUpUser() {
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            if let error = error {
-                errorMessage = "Falló registrarse: \(error.localizedDescription)"
-                return
-            }
-            // Guardar datos del usuario en Firestore
-            guard let uid = result?.user.uid else { return }
-            let userData = ["email": email]
-            Firestore.firestore().collection("users").document(uid).setData(userData) { error in
-                if let error = error {
-                    errorMessage = "Fallo guardar datos de usuario: \(error.localizedDescription)"
-                    return
-                }
-                // Manejar registro exitoso
-                errorMessage = "Registro exitoso"
-            }
-        }
-    }
-}
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-    }
-}
-*/
-import SwiftUI
-import FirebaseAuth
-import FirebaseFirestore
-
-struct LoginView: View {
-    @State private var email: String = ""
-    @State private var password: String = ""
-    @State private var isLoginMode: Bool = true
-    @State private var errorMessage: String = ""
     @Binding var isUserLoggedIn: Bool
     
     var body: some View {
@@ -188,5 +96,134 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView(isUserLoggedIn: .constant(false))
+    }
+}
+*/
+
+import SwiftUI
+import FirebaseAuth
+import FirebaseFirestore
+import GoogleSignIn
+
+struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var isLoginMode: Bool = true
+    @State private var errorMessage: String = ""
+    @Binding var isUserLoggedIn: Bool
+    
+    var body: some View {
+        VStack {
+            Picker(selection: $isLoginMode, label: Text("Modo Registro")) {
+                Text("Iniciar Sesión").tag(true)
+                Text("Registrarse").tag(false)
+            }.pickerStyle(SegmentedPickerStyle())
+                .padding()
+
+            TextField("Email", text: $email)
+                .keyboardType(.emailAddress)
+                .autocapitalization(.none)
+                .padding()
+
+            SecureField("Contraseña", text: $password)
+                .padding()
+
+            Button(action: {
+                handleAction()
+            }) {
+                Text(isLoginMode ? "Iniciar Sesión" : "Registrarse")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+            
+            Button(action: {
+                handleGoogleSignIn()
+            }) {
+                HStack {
+                    Image(systemName: "globe")
+                    Text("Iniciar con Google")
+                }
+            }
+            .padding()
+            
+            Text(errorMessage)
+                .foregroundColor(.red)
+                .padding()
+        }
+        .padding()
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            loginUser()
+        } else {
+            signUpUser()
+        }
+    }
+    
+    private func loginUser() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                errorMessage = "Falló iniciar sesión: \(error.localizedDescription)"
+                return
+            }
+            isUserLoggedIn = true
+        }
+    }
+    
+    private func signUpUser() {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                errorMessage = "Falló registrarse: \(error.localizedDescription)"
+                return
+            }
+            guard let uid = result?.user.uid else { return }
+            let userData = ["email": email]
+            Firestore.firestore().collection("users").document(uid).setData(userData) { error in
+                if let error = error {
+                    errorMessage = "Fallo guardar datos de usuario: \(error.localizedDescription)"
+                    return
+                }
+                isUserLoggedIn = true
+            }
+        }
+    }
+    
+    private func handleGoogleSignIn() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        let config = GIDConfiguration(clientID: clientID)
+        
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: getRootViewController()) { user, error in
+            if let error = error {
+                errorMessage = "Falló iniciar sesión con Google: \(error.localizedDescription)"
+                return
+            }
+            guard let authentication = user?.authentication,
+                  let idToken = authentication.idToken else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            Auth.auth().signIn(with: credential) { result, error in
+                if let error = error {
+                    errorMessage = "Falló iniciar sesión con Google: \(error.localizedDescription)"
+                    return
+                }
+                isUserLoggedIn = true
+            }
+        }
+    }
+    
+    private func getRootViewController() -> UIViewController {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let rootViewController = windowScene.windows.first?.rootViewController else {
+            return UIViewController()
+        }
+        return rootViewController
     }
 }
