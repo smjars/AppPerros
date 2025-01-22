900a583668588658b081ee2d02123a4da1adb0e3
Josue Ruiz <JosueRuiz.com@gmail.com>
2025-01-22 20:00:40 +0100

    agregados los datos del animal a adoptar y todo funciona, falta que se redirija hacia la lista principal.

--- AppPerros/Views/AnimalListView.swift
+++ AppPerros/Views/AnimalListView.swift
@@ -5,73 +5,6 @@
 //  Created by Tardes on 21/1/25.
 //
 
-/*
-
-import SwiftUI
-import FirebaseAuth
-
-struct AnimalListView: View {
-    @State private var animals: [Animal] = []
-    private let apiService = ApiService()
-    @Binding var isUserLoggedIn: Bool
-
-    var body: some View {
-        NavigationView {
-            List(animals) { animal in
-                NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                    HStack {
-                        AsyncImage(url: URL(string: animal.imagen)) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                        } placeholder: {
-                            Color.gray
-                        }
-                        .frame(width: 120, height: 120)
-                        .clipShape(Rectangle())
-
-                        VStack(alignment: .leading) {
-                            Text(animal.nombre)
-                                .font(.headline)
-                            Text(animal.tipo)
-                                .font(.subheadline)
-                        }
-                    }
-                }
-            }
-            .navigationTitle("Animales en Adopción")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Cerrar Sesión") {
-                        logout()
-                    }
-                }
-            }
-        }
-        .onAppear {
-            apiService.fetchAnimals { animals in
-                self.animals = animals
-            }
-        }
-    }
-
-    private func logout() {
-        do {
-            try Auth.auth().signOut()
-            isUserLoggedIn = false
-        } catch let signOutError as NSError {
-            print("Error al cerrar sesión: %@", signOutError)
-        }
-    }
-}
-
-struct AnimalListView_Previews: PreviewProvider {
-    static var previews: some View {
-        AnimalListView( isUserLoggedIn: .constant(true))
-    }
-}
-*/
-
 import SwiftUI
 import FirebaseAuth
 
@@ -114,6 +47,7 @@ struct AnimalListView: View {
                         }
                     }
                 }
+                .padding(.top, -70)
                 Text("Correo: \(userEmail)")
                     .padding()
             }
